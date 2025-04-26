1869cb618c43b02c4d634449d05193c764c443d3
LongVu7 <vuvlgcs220799@fpt.edu.vn>
2025-04-27 04:17:40 +0700

    Project

---
+++ Project/index.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+require_once 'conn.php';
+require_once 'includes/DatabaseConnection.php';
+require_once 'includes/DatabaseFunctions.php';
+
+if(!isset($_SESSION['user'])){
+    header('location: login.html.php');
+    exit();
+}
+
+$id = $_SESSION['user'];
+$sql = $conn->prepare("SELECT * FROM `author` WHERE `id`=?");
+$sql->execute(array($id));
+$fetch = $sql->fetch();
+
+// Get totals for display
+$totalQuestions = totalQuestion($pdo);
+$totalUsers = totalUser($pdo);
+
+$title = 'CourWork A Forum for Learning';
+ob_start();
+include 'templates/home.html.php';
+$output = ob_get_clean();
+include 'templates/layout.html.php';
---
+++ Project/templates/home.html.php
@@ -0,0 +1,45 @@
+<p>This website established to generate a environment for user who has enthusiasm to learning</p>
+
+<div class="row">
+    <!-- Questions Card -->
+    <div class="col-sm-6 col-md-3">
+        <div class="card card-stats card-round">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-icon">
+                        <div class="icon-big text-center icon-primary bubble-shadow-small">
+                            <i class="fas fa-book-open"></i>
+                        </div>
+                    </div>
+                    <div class="col col-stats ms-3 ms-sm-0">
+                        <div class="numbers">
+                            <p class="card-category">Total Questions</p>
+                            <h4 class="card-title"><?= $totalQuestions ?></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Users Card -->
+    <div class="col-sm-6 col-md-3">
+        <div class="card card-stats card-round">
+            <div class="card-body">
+                <div class="row align-items-center">
+                    <div class="col-icon">
+                        <div class="icon-big text-center icon-primary bubble-shadow-small">
+                            <i class="fas fa-users"></i>
+                        </div>
+                    </div>
+                    <div class="col col-stats ms-3 ms-sm-0">
+                        <div class="numbers">
+                            <p class="card-category">Total Users</p>
+                            <h4 class="card-title"><?= $totalUsers ?></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
